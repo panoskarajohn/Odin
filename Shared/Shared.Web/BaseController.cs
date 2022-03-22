b7eea495fe18907b323ad5f09c8dcad3db1ad4de
panoskarajohn <panoskarajohn@hotmail.com>
2022-03-22 23:22:47 +0200

    Base controller and Id generator created

---
+++ Shared/Shared.Web/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Cqrs;
+
+namespace Shared.Web;
+
+public abstract class BaseController : ControllerBase
+{
+    protected const string BaseApiPath = "api/v{version:apiVersion}";
+    private IMapper _mapper;
+    private IDispatcher _dispatcher;
+    
+    protected IDispatcher Dispatcher =>
+        _dispatcher ??= HttpContext.RequestServices.GetService<IDispatcher>()!;
+
+    protected IMapper Mapper => 
+        _mapper ??= HttpContext.RequestServices.GetService<IMapper>()!;
+}
