8cca712069e962e8983b6109cddacc350fbdedb2
panoskarajohn <panoskarajohn@hotmail.com>
2022-03-30 21:20:40 +0300

    Updated response to contain market as well

--- src/Services/Event/Event.Application/Match/Dtos/MatchResponseDto.cs
+++ src/Services/Event/Event.Application/Match/Dtos/MatchResponseDto.cs
@@ -1,5 +1,6 @@
 ﻿namespace Event.Application.Match.Dtos;
 
-public record MatchResponseDto(long Id, string Category, DateTime StartingTime, string MatchName)
+public record MatchResponseDto(long Id, string Category, DateTime StartingTime, string MatchName,
+    IEnumerable<MarketDto> MarketDtos)
 {
 }
--- src/Services/Event/Event.Application/Match/Mapping/MatchDocumentToResponse.cs
+++ src/Services/Event/Event.Application/Match/Mapping/MatchDocumentToResponse.cs
@@ -10,6 +10,18 @@ public static class MatchDocumentToResponse
         return new MatchResponseDto(document.Id,
             document.Category,
             document.StartingTime,
-            document.MatchName);
+            document.MatchName,
+            document.Markets.Select(m => m.ToDto()));
+    }
+
+    public static MarketDto ToDto(this MarketDocument document)
+    {
+        return new MarketDto(document.Name,
+            document.SelectionDocuments.Select(s => s.ToDto()));
+    }
+
+    public static SelectionDto ToDto(this SelectionDocument document)
+    {
+        return new SelectionDto(document.Name, document.Price);
     }
 }
