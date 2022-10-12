0270e8abcaa0b851d70fad3b720a0131c62c7cd7
Panos <panoskarohn@hotmail.com>
2022-10-12 16:00:43 +0300

    Added event grpc and fixed some issues with the sh scripts that needed some special configuration since I run them from windowns

--- Deployments/start_infra.sh
+++ Deployments/start_infra.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # clean all containers and volumes
-docker stop $(docker ps -a -q)
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
+#docker stop $(docker ps -a -q)
+#docker rm -f $(docker ps -a -q)
+#docker volume rm $(docker volume ls -q)
 
 docker-compose --file ../infra.yml up -d
 
--- src/Services/Slip/Slip.Api/Program.cs
+++ src/Services/Slip/Slip.Api/Program.cs
@@ -1,4 +1,12 @@
+using Shared.Metrics;
+using Shared.Prometheus;
+using Shared.Swagger;
+using Shared.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+var host = builder.Host;
+var env = builder.Environment;
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication(configuration)
+    .AddErrorHandling()
+    .AddMetrics()
+    .AddPrometheus(configuration)
+    .AddCustomSwagger(configuration, typeof(ISlipMarker).Assembly)
+    .AddCustomVersioning();
+
 
 var app = builder.Build();
 
