9e3bfa093ad5f9e4e26fe8b0f338e244f2e0926e
Panos <panoskarohn@hotmail.com>
2022-12-13 19:36:30 +0200

    Added shared jwt options

---
+++ src/Shared/Shared.Jwt/AuthHeaderHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+
+namespace Shared.Jwt;
+
+public class AuthHeaderHandler : DelegatingHandler
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+    public AuthHeaderHandler(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var token = (_contextAccessor?.HttpContext?.Request.Headers["Authorization"])?.ToString();
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token?.Replace("Bearer ", "", StringComparison.Ordinal));
+
+        return base.SendAsync(request, cancellationToken);
+    }
+} 
+   
---
+++ src/Shared/Shared.Jwt/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using Duende.IdentityServer.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Shared.Common;
+
+namespace Shared.Jwt;
+
+public static class Extensions
+{
+    public static IServiceCollection AddJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtOptions = configuration.GetOptions<JwtBearerOptions>("Jwt");
+        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,options =>
+            {
+                options.Authority = jwtOptions.Authority;
+                options.TokenValidationParameters.ValidateAudience = false;
+            });
+
+        if (!string.IsNullOrEmpty(jwtOptions.Audience))
+        {
+            services.AddAuthorization(option =>
+            {
+                option.AddPolicy(nameof(ApiScope), policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", jwtOptions.Audience);
+                });
+            });
+        }
+        
+        return services;
+    }
+    
+}
